82c93c3bc24c3bedc1177079b7ceeefa6238172c
zer <zach.earos@gmail.com>
2016-01-28 15:54:49 +0000

    working for registering from T1 seg

---
+++ RegSeg/Register_Resample_Segs_From_T1.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import Diff_Preprocess_Defs as DPD
+
+#This registers the synthetic signal to the real one and adjusts the segmentations. 
+
+#topDir = '/Users/zer/RegFitAD/data/Ep/Testing_RegSeg'
+topDir = '/Users/zer/RegFitAD/data/Ep/TestT1'
+T1Dir = '/Users/zer/RegFitAD/data/Ep/T1'
+RAWdir = os.path.join(topDir,'Raw')
+refIm = os.path.join(RAWdir,'DT_MD.nii.gz')
+rMask = os.path.join(RAWdir,'Brain_mask.nii.gz')
+dirBase = 'it_0'
+
+for i in range(50):
+	#NAMING 
+	itDir = os.path.join(topDir,dirBase +str(i))
+	nextDir = os.path.join(topDir,dirBase +str(i+1))
+	prevDir = os.path.join(topDir,dirBase +str(i-1))
+
+	floIm = os.path.join(itDir,'T1MD.nii.gz')
+	resIm = os.path.join(itDir,'MDregged.nii.gz')
+	affIn = os.path.join(topDir,'aff.txt')
+	cppName = os.path.join(itDir,'trans.cpp.nii')
+	segResName = os.path.join(nextDir,'Segs_Diffspace.nii.gz')
+
+	if os.path.isdir(itDir) and not os.path.isdir(nextDir) and os.path.isfile(floIm):
+		os.mkdir(nextDir)
+
+	if not os.path.isfile(resIm) and os.path.isfile(floIm):
+		other_args = '-rmask ' + rMask
+		DPD.NR_reg(refIm,floIm,cppName,resIm,dbg=False,other_args = other_args)
+
+	T1segName = os.path.join(T1Dir,'Segmentation.nii.gz')
+	if not os.path.isfile(segResName) and os.path.isfile(cppName):
+		DPD.reg_resample(refIm,T1segName,segResName,cpp = cppName,dbg = False,other_args = '-inter 4 -psf')
+
+	oldName = os.path.join(itDir,'reg_results.txt')
+	newName = os.path.join(nextDir,'init_params.txt')
+	if not os.path.isfile(newName) and os.path.isfile(oldName):
+		shutil.copyfile(oldName,newName)
+
+
--- RegSeg/fit_with_s0_as_param.m
+++ RegSeg/fit_with_s0_as_param.m
@@ -1,12 +1,14 @@
 function fit_with_s0_as_param()
 
-subjDir = '/Users/zer/RegFitAD/data/Ep/TestS0';
+subjDir = '/Users/zer/RegFitAD/data/Ep/TestT1';
 baseName = 'it_0';
+T1Dir = '/Users/zer/RegFitAD/data/Ep/T1'; 
 
-for i = 0:50
-run_reg_fit_wholebrain(subjDir,i,baseName);
+for i = 1:20
+run_reg_fit_wholebrain(subjDir,i,baseName,T1Dir);
 bash_profile = 'source ~/.bash_profile; ';
-pyCMD = 'python /Users/zer/RegFitAD/code/RegSeg/Register_Resample_Segs.py';
+%pyCMD = 'python /Users/zer/RegFitAD/code/RegSeg/Register_Resample_Segs.py';
+pyCMD = 'python /Users/zer/RegFitAD/code/RegSeg/Register_Resample_Segs_From_T1.py';
 system([bash_profile,pyCMD]);
 
 end
@@ -14,50 +16,47 @@ end
 
 end
 
-function run_reg_fit_wholebrain( subjDir ,itNum,baseName)
+function run_reg_fit_wholebrain( subjDir ,itNum,baseName,T1Dir)
 %This runs the regional fit on the whole-brain, trimming none of it.
+DWfac = 1e6;
 
 itDir = fullfile(subjDir,[baseName,num2str(itNum)]);
 RAWdir = fullfile(subjDir,'Raw'); 
 
 %adding paths:
-%addpath(genpath('/home/zeatonro/NIfTI_20140122'));
-%addpath('/home/zeatonro/MultiProd');
 addpath('/Users/zer/RegFitAD/code')
 
-%cortParcName = fullfile(subjDir,'T1_Diffspace','Lobes_Diffspace.nii.gz');
-
-
 [DWname,V1Name,V2Name,bMaskName] = get_permanent_fnames(RAWdir);
-
 
 %outNames 
 segName = fullfile(itDir,'Segs_Diffspace.nii.gz');
 outName = fullfile(itDir,'reg_results.txt');
 
-%loading
-Segs = load_untouch_nii(segName);
-%getting rid of unphysically small values that mess up normalisation.
-Segs.img(Segs.img < 1e-3) = 0;
+%loading segmentations
 
 bMask = load_untouch_nii(bMaskName);bMask = bMask.img >.5; 
-
-%1st dimension is irrelevant. 
-
-Segs = normalise_segs(Segs); Segs = Segs.img;
-DW = load_untouch_nii(DWname); DW = DW.img./1e6;
+DW = load_untouch_nii(DWname); DW = DW.img./DWfac;
 bvals = load(fullfile(RAWdir,'bvals'));
 bvecs = load(fullfile(RAWdir,'bvecs'));
 
+W = flat_segs(segName,bMask); 
 
 %%
+
 [DW,bvals,bvecs] = remove_high_bvals(DW,bvals,bvecs);
 
 [bMat,bMask] = prepare_b_matrices(V1Name,V2Name,bMask,bvals,bvecs);
 
+%% T1 stuff 
+T1MaskName = fullfile(T1Dir,'Brain_mask.nii.gz'); T1Mask = load_untouch_nii(T1MaskName);
+T1Mask = T1Mask.img>.5; 
+T1Name = fullfile(T1Dir,'T1.nii.gz');
+
+T1SegName = fullfile(T1Dir,'Segmentation.nii.gz'); 
+W_T1 = flat_segs(T1SegName,T1Mask);
+
 
 %%
-W = flattener_4d(Segs,bMask);
 DW = flattener_4d(DW,bMask);
 
 %%
@@ -66,14 +65,14 @@ initParamName = fullfile(itDir,'init_params.txt');
 if exist(initParamName,'file')
     initParams = load(initParamName);
 else
-    initParams = repmat([.001, 1.7e-3, 1.2e-3,1.1e-3],[k 1]);
+    initParams = repmat([1e3./DWfac, 1.7e-3, 1.2e-3,1.1e-3],[k 1]);
 end
 
 
 %%
 %note: the Rician noise is added after the scaling by S0, so it is still
 %correct and is not a percentage of the signal at a voxel.
-riceNoise = 0 ;%sqrt(13); 
+riceNoise = 0;%sqrt(13./DWfac); 
 SSDind = 's'; %1,0,'s'
 %note- this is a tad high, but there's excellent immunity to it.. .
 
@@ -86,12 +85,31 @@ if ~exist(outName,'file')
     
     %% making the synthetic ims
     
-    imHolder = load_untouch_nii(DWname);
+    outSynthName = fullfile(itDir,'DWsynth.nii.gz');
+    save_synth_DW_lo(DWname,bMask,sigGuess,outSynthName);
+    save_param_maps_high(T1Name,T1Mask,W_T1,paramVals,itDir);
     
+end
+
+end
+
+function W = flat_segs(segName,bMask)
+
+Segs = load_untouch_nii(segName);
+%getting rid of unphysically small values that mess up normalisation.
+Segs.img(Segs.img < 1e-3) = 0;
+Segs = normalise_segs(Segs); Segs = Segs.img;
+W = flattener_4d(Segs,bMask);
+
+
+end
+
+%Saves the low-resolution diffusion data 
+function save_synth_DW_lo(DWname,bMask,sigGuess,outSynthName)
+    
+    imHolder = load_untouch_nii(DWname);
     blankIm = zeros(size(bMask,1),size(bMask,2),size(bMask,3),size(sigGuess,2));
     synthDW = imHolder;   synthDW.img = blankIm;
-    
-    outSynthName = fullfile(itDir,'DWsynth.nii.gz');
     
     for it = 1:size(sigGuess,2)
         tmp  = zeros(size(bMask));
@@ -99,13 +117,37 @@ if ~exist(outName,'file')
         synthDW.img(:,:,:,it) = tmp;
     end
     
-    
     synthDW.hdr.dime.dim(2:5) = size(synthDW.img);
     save_untouch_nii(synthDW,outSynthName);
 end
 
-end
+function save_param_maps_high(T1Name,T1Mask,W_T1,paramVals,itDir)
+    
+    paramVals(:,1) = []; 
+    MD = mean(paramVals,2); 
 
+    tmp = (paramVals - repmat(MD,[1 3])).^2;
+    FA = sqrt(1.5) .* sqrt( sum(tmp,2))./sqrt(sum(paramVals.^2,2));
+    
+    imHolder = load_untouch_nii(T1Name); 
+
+    FAim = zeros(size(imHolder.img));
+    FAim(T1Mask) = W_T1 * FA; 
+    FAmap = imHolder; FAmap.img = single(FAim); 
+    FAmap.hdr.dime.cal_max=0.8; 
+    
+    MDim = zeros(size(imHolder.img));
+    MDim(T1Mask) = W_T1 * MD; 
+    MDmap = imHolder; MDmap.img = single(MDim); 
+    MDmap.hdr.dime.cal_max=0.0025; 
+    
+    FAname = fullfile(itDir,'T1FA.nii.gz');
+    MDname = fullfile(itDir,'T1MD.nii.gz');
+    
+    save_untouch_nii(FAmap,FAname);
+    save_untouch_nii(MDmap,MDname); 
+
+end
 
 function  [paramVals,sigGuess]  = do_fit(DW,W,bMat,initParams,sig,SSDind)
 %DIRECT_FIT_DT This fits the DT model in the region-wise manner. It does
@@ -192,8 +234,8 @@ end
 function [DWname,V1Name,V2Name,bMaskName] = get_permanent_fnames(RAWdir)
 %These never change - still fitting to the original data. 
 DWname = fullfile(RAWdir,'DW.nii.gz');
-V1Name = fullfile(RAWdir,'DT','DT_V1.nii.gz');
-V2Name = fullfile(RAWdir,'DT','DT_V2.nii.gz');
+V1Name = fullfile(RAWdir,'DT_V1.nii.gz');
+V2Name = fullfile(RAWdir,'DT_V2.nii.gz');
 bMaskName = fullfile(RAWdir,'Brain_Mask.nii.gz');
 
 end
